d8bb7d762c613ee41722dbfd55a4313c9df1fc11
Roger Cedeño <grarogccee@gamil.com>
2020-02-15 14:18:37 -0500

    vistas cambios

---
+++ front_end/react_web/src/components/header.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from'react';
+import { Link } from "react-router-dom";
+
+const Header = () => (
+        <div className="flex-grow flex mr-2 ml-64">
+            <div className="flex-grow flex">
+                {/* <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+                    Ayuda
+                </a> */}
+            </div>
+            <div>
+                <Link to="/">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                      Salir
+                    </button>
+                </Link>
+            </div>            
+        </div>
+)
+
+export default Header;
